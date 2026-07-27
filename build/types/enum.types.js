4c43c6943718557a8527146044b7358b42d961bb
amir.r10 <amir.poudel2000@gmail.com>
2026-07-27 13:50:35 +0545

    build removed

--- build/types/enum.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["USER"] = "USER";
-    Role["ADMIN"] = "ADMIN";
-    Role["SUPER_ADMIN"] = "SUPER ADMIN";
-})(Role || (exports.Role = Role = {}));
